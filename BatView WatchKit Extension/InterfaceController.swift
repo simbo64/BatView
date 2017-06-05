f8e9e9993ea15377552923ea19d8d32b847b5b2d
Simon Edwards <simon.nomis@hotmail.co.uk>
2017-06-05 23:02:01 +0100

    Initial Commit

---
+++ BatView WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BatView WatchKit Extension
+//
+//  Created by Simon Edwardes on 05/06/2017.
+//  Copyright © 2017 Simon Edwardes. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ BatView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BatView
+//
+//  Created by Simon Edwardes on 05/06/2017.
+//  Copyright © 2017 Simon Edwardes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
